5c7c19169ed2f9a9dadae9f36380dd0d2fb6c7bc
magacek <70607808+magacek@users.noreply.github.com>
2023-09-11 16:43:25 -0400

    project 2 updated

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -1,9 +1,12 @@
+
 package com.example.project1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+// MainActivity that extends AppCompatActivity (Android class for handling activities)
 
 /**
  * MainActivity serves as the primary activity for the calculator application.
@@ -29,22 +32,35 @@ import android.widget.TextView
  * @version 1.0
  * @since 9/11/2023
  */
-class MainActivity : AppCompatActivity() {
 
+
+class MainActivity : AppCompatActivity() {
     private lateinit var displayTextView: TextView
     private val calculations = mutableListOf<Any>()  // Used to accumulate numbers and operations
     private var newOperation = false
+    // Saving instance state (like during screen rotation)
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("displayText", displayTextView.text.toString())
+        outState.putBoolean("newOperation", newOperation)
+        outState.putStringArrayList("calculations", calculations.map { it.toString() } as ArrayList<String>)
+    }
+
+    // Creating the activity view
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         displayTextView = findViewById(R.id.displayTextView)
+
+        // List of number buttons' IDs
 
         val numberButtons = listOf(
             R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4,
             R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9
         )
+        // Setting click listeners for number buttons
 
         for (id in numberButtons) {
             val button: Button = findViewById(id)
@@ -53,9 +69,12 @@ class MainActivity : AppCompatActivity() {
                     displayTextView.text = ""
                     newOperation = false
                 }
+                Log.d("Button Click", "Button ${button.text} clicked")
                 onNumberClicked((it as Button).text.toString())
             }
         }
+
+        // Map of operation buttons and their symbols
 
         val operations = mapOf(
             R.id.buttonPlus to "+",
@@ -66,16 +85,59 @@ class MainActivity : AppCompatActivity() {
 
         for ((id, operation) in operations) {
             val button: Button = findViewById(id)
-            button.setOnClickListener { onOperationClicked(operation) }
+            button.setOnClickListener {
+                Log.d("Button Click", "Button ${button.text} clicked")
+                onOperationClicked(operation) }
         }
 
-        findViewById<Button>(R.id.buttonEquals).setOnClickListener { calculateResult() }
-        findViewById<Button>(R.id.buttonC).setOnClickListener { resetCalculator() }
-        findViewById<Button>(R.id.buttonDot).setOnClickListener { onDotClicked() }
-        findViewById<Button>(R.id.buttonPlusMinus).setOnClickListener { toggleSign() }
-        findViewById<Button>(R.id.buttonPercent).setOnClickListener { percentage() }
+        findViewById<Button>(R.id.buttonEquals).setOnClickListener {
+            Log.d("Equal", "Button = Equal")
+            calculateResult()
+        }
+        findViewById<Button>(R.id.buttonC).setOnClickListener {
+            Log.d("Reset", "Button = Reset")
+            resetCalculator() }
+        findViewById<Button>(R.id.buttonDot).setOnClickListener {
+            Log.d("Dot", "Button = Dot")
+            onDotClicked() }
+        findViewById<Button>(R.id.buttonPlusMinus).setOnClickListener {
+            Log.d("Sign change", "Button = Sign")
+            toggleSign() }
+        findViewById<Button>(R.id.buttonPercent).setOnClickListener {
+            Log.d("Percentage", "Button = Percentage")
+            percentage() }
+
+        findViewById<Button>(R.id.buttonSin)?.setOnClickListener {
+            Log.d("Sin", "Button = Sin")
+            computeSin() }
+        findViewById<Button>(R.id.buttonCos)?.setOnClickListener {
+            Log.d("Cos", "Button = Cos")
+            computeCos() }
+        findViewById<Button>(R.id.buttonTan)?.setOnClickListener {
+            Log.d("Tan", "Button = Tan")
+            computeTan() }
+        findViewById<Button>(R.id.buttonLog10)?.setOnClickListener {
+            Log.d("Log10", "Button = Log10")
+            computeLog10() }
+        findViewById<Button>(R.id.buttonLn)?.setOnClickListener {
+            Log.d("Ln", "Button = Ln")
+            computeLn() }
+        if (savedInstanceState != null) {
+            newOperation = savedInstanceState.getBoolean("newOperation")
+            displayTextView.text = savedInstanceState.getString("displayText")
+            calculations.addAll(savedInstanceState.getStringArrayList("calculations")?.map {
+                when {
+                    it.isDouble() -> it.toDouble()
+                    it.isOperation() -> it
+                    else -> null
+                }
+            }?.filterNotNull() ?: listOf())
+        }
+
     }
 
+    private fun String.isDouble(): Boolean = this.toDoubleOrNull() != null
+    private fun String.isOperation(): Boolean = this in listOf("+", "-", "X", "/")
     private fun onNumberClicked(number: String) {
         if (displayTextView.text == "0" || newOperation) {
             displayTextView.text = number
@@ -84,7 +146,6 @@ class MainActivity : AppCompatActivity() {
             displayTextView.text = "${displayTextView.text}$number"
         }
     }
-
     private fun onDotClicked() {
         if (!displayTextView.text.contains(".")) {
             displayTextView.text = "${displayTextView.text}."
@@ -146,8 +207,7 @@ class MainActivity : AppCompatActivity() {
             else -> 0.0
         }
     }
-
-
+    // Helper function to toggle the sign of the displayed number
 
     private fun formatResult(result: Double?): String {
         if (result == null) return "Error"
@@ -162,18 +222,69 @@ class MainActivity : AppCompatActivity() {
     private fun resetCalculator() {
         displayTextView.text = "0"
     }
+    // Setting listeners for trigonometric and logarithmic functions
+
 
     private fun toggleSign() {
         val currentValue = displayTextView.text.toString().toDoubleOrNull()
-        if (currentValue != null) {
+        if (currentValue != null && !currentValue.isNaN() && !currentValue.isInfinite()) {
             displayTextView.text = (-currentValue).toString()
+        } else {
+            displayTextView.text = "Error"
         }
     }
 
-    private fun percentage() {
-        val currentValue = displayTextView.text.toString().toDoubleOrNull()
-        if (currentValue != null) {
-            displayTextView.text = (currentValue / 100).toString()
+    private fun computeCos() {
+        val value = displayTextView.text.toString().toDoubleOrNull()
+        if (value != null) {
+            displayTextView.text = Math.cos(Math.toRadians(value)).toString()
         }
     }
+    // Helper functions for trigonometric and logarithmic calculations
+
+    private fun computeSin() {
+        val value = displayTextView.text.toString().toDoubleOrNull()
+        if (value != null) {
+            displayTextView.text = Math.sin(Math.toRadians(value)).toString()
+        } else {
+            displayTextView.text = "Error"
+        }
+    }
+    private fun computeTan() {
+        val value = displayTextView.text.toString().toDoubleOrNull()
+        if (value != null && (value % 90).toInt() != 0) { // Avoiding tan(90), tan(270), etc.
+            displayTextView.text = Math.tan(Math.toRadians(value)).toString()
+        } else {
+            displayTextView.text = "Error"
+        }
+    }
+
+    private fun computeLog10() {
+        val value = displayTextView.text.toString().toDoubleOrNull()
+        if (value != null && value > 0) {
+            displayTextView.text = Math.log10(value).toString()
+        } else {
+            displayTextView.text = "Error"
+        }
+    }
+
+    private fun computeLn() {
+        val value = displayTextView.text.toString().toDoubleOrNull()
+        if (value != null && value > 0) {
+            displayTextView.text = Math.log(value).toString()
+        } else {
+            displayTextView.text = "Error"
+        }
+    }
+
+
+    private fun percentage() {
+        val currentValue = displayTextView.text.toString().toDoubleOrNull()
+        if (currentValue != null && !currentValue.isNaN() && !currentValue.isInfinite()) {
+            displayTextView.text = (currentValue / 100).toString()
+        } else {
+            displayTextView.text = "Error"
+        }
+    }
+
 }
